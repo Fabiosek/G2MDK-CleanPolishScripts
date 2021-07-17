c0e888c7a5b0d85b4cb95194913e06aff543e3f4
Fabio <thedarkteamofficialyt@gmail.com>
2021-07-17 16:29:12 +0200

    Update B_Say_GuildGreetings.d

--- Content/AI/Human/B_Human/B_Say_GuildGreetings.d
+++ Content/AI/Human/B_Human/B_Say_GuildGreetings.d
@@ -24,20 +24,7 @@ func void B_Say_GuildGreetings (var C_NPC slf, var C_NPC oth)
 		B_Say_Overlay (slf, oth, "$PALGREETINGS");
 		return;
 	};
-		
-	/*if (Hlp_GetInstanceID(slf) == Hlp_GetInstanceID(Bartok))
-	&& (Bartok_OrkGesagt == TRUE)
-	{
-		//B_Bartok_ShitAnOrc(); //FIXME COSMETIC - sollte besser SVM_Overlay sein
-		return;
-	};
-	
-	if (Hlp_GetInstanceID(slf) == Hlp_GetInstanceID(Koch))
-	{
-		//B_Koch_Hackebeil(); //FIXME COSMETIC - sollte besser SVM_Overlay sein
-		return;
-	};*/
-		
+			
 	var int zufall; zufall = Hlp_Random (100); 
 	
 	if (zufall <= 10)
@@ -45,8 +32,5 @@ func void B_Say_GuildGreetings (var C_NPC slf, var C_NPC oth)
 	{
 		B_Say_Overlay (slf, oth, "$WEATHER"); //So ein Mistwetter!
 		return;
-		
 	};
 };
-
-
